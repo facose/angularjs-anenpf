586d87cdd735b5b4279a19df0cfc012e965f9820
facose <fabianconejosegu@gmail.com>
2021-04-15 09:17:55 -0600

    2 files modified

--- home/home.controller.js
+++ home/home.controller.js
@@ -3,12 +3,16 @@ class HomeCtrl {
     // Declarating array of people objects
     $scope.people = [];
 
-    // Method for adding new people. Check input fields on html for reference on addName and addLastName.
+    // Method for adding new people if input boxes are not empty. Check input fields on html for reference on addName and addLastName.
     $scope.add = () => {
-      $scope.people.push({
-        name: `${$scope.addName}`,
-        lastName: `${$scope.addLastName}`
-      });
+      if (!$scope.addName || !$scope.addLastName) {
+        return;
+      } else {
+        $scope.people.push({
+          name: `${$scope.addName}`,
+          lastName: `${$scope.addLastName}`
+        });
+      }
 
       // Clearing the input boxes after pushing new obj to array
       $scope.addName = "";
